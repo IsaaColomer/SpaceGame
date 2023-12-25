64b36c7822f2a68e4e931f1aa3a091b7fcbc467a
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-12-25 12:50:41 +0100

    saddas

---
+++ SpaceGame/Assets/_Stuff/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    
+    public bool CheckIfObjectCanBeInteracted(GameObject go)
+    {
+        bool ret = false;
+        return ret;
+    }
+}
