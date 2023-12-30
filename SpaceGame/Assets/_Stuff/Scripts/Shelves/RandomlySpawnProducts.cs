87f01eeb68c053b4603794d4a595648ea81ffec3
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-12-30 13:11:17 +0100

    added randomly generate products to shelves

---
+++ SpaceGame/Assets/_Stuff/Scripts/Shelves/RandomlySpawnProducts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RandomlySpawnProducts : MonoBehaviour
+{
+    public List<GameObject> products = new List<GameObject>();
+    private List<GameObject> possibleSpawns = new List<GameObject>();
+
+    private void Start()
+    {
+        foreach (Transform p in transform)
+        {
+            if(p.CompareTag("SpawnPosition"))
+            {
+                possibleSpawns.Add(p.gameObject);
+            }
+        }
+        Debug.Log("Total ammount of possible spawns: " + possibleSpawns.Count);
+
+        for(int  i = 0; i < possibleSpawns.Count; ++i)
+        {
+            if(Random.Range(0,2) == 1)
+            {
+                Instantiate(products[Random.Range(0, products.Count+1)], possibleSpawns[i].transform.position, Quaternion.identity, possibleSpawns[i].transform);
+            }
+        }
+    }
+}
