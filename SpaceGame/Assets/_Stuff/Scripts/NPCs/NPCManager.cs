6da5271f144c299b1fb4edf7db42d1de3a913365
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-12-31 18:17:21 +0100

    a
    a

--- SpaceGame/Assets/_Stuff/Scripts/NPCs/NPCManager.cs
+++ SpaceGame/Assets/_Stuff/Scripts/NPCs/NPCManager.cs
@@ -12,27 +12,14 @@ public class NPCManager : MonoBehaviour
     // only for killing Animation
     private CinemachineVirtualCamera virtualCamera;
     private CinemachineBasicMultiChannelPerlin perlinNoise;
-
-    List<GameObject> go = new List<GameObject>();
-    List<Transform> got = new List<Transform>();
-    //
-
-    public float minDistanceForArguing = 2f;
-
     private void Awake()
     {
         anim = GetComponent<Animator>();
         virtualCamera = GameObject.Find("PlayerVirtualCamera").GetComponent<CinemachineVirtualCamera>();
         perlinNoise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        go = GameObject.FindGameObjectsWithTag("NPC").ToList();
-        foreach (GameObject t in go)
-        {
-            got.Add(t.transform);
-        }
     }
     private void Start()
     {
-        DecideIfArgue();
         ResetIntensity();
     }
     public void KillNPC()
@@ -63,59 +50,5 @@ public class NPCManager : MonoBehaviour
     public void DestroyNPC()
     {
         Destroy(this.gameObject);
-    }
-    public void DecideIfArgue()
-    {
-        foreach (GameObject tmp in go)
-        {
-            if(tmp != this.gameObject)
-            {
-                if((tmp.transform.position - this.gameObject.transform.position).magnitude < minDistanceForArguing)
-                {
-                    anim.SetBool("isSomeOneClose", true);
-                    transform.LookAt(tmp.transform.position);
-                }
-            }
-        }
-    }
-    
-    Transform GetClosestNPC(List<Transform> enemies)
-    {
-        Transform tMin = null;
-        float minDist = Mathf.Infinity;
-        Vector3 currentPos = transform.position;
-        foreach (Transform t in enemies)
-        {
-            float dist = Vector3.Distance(t.position, currentPos);
-            if (dist < minDist)
-            {
-                tMin = t;
-                minDist = dist;
-            }
-        }
-        return tMin;
-    }
-    private void FixedUpdate()
-    {
-        DecideIfArgue();
-    }
-
-    private void Update()
-    {
-        List<GameObject> go = GameObject.FindGameObjectsWithTag("NPC").ToList();
-        foreach (GameObject tmp in go)
-        {
-            if (tmp != this.gameObject)
-            {
-                if((tmp.transform.position - this.gameObject.transform.position).magnitude < minDistanceForArguing)
-                {
-                    Debug.DrawLine(this.gameObject.transform.position, tmp.transform.position, Color.green);
-                }
-                else
-                {
-                    Debug.DrawLine(this.gameObject.transform.position, tmp.transform.position, Color.red);
-                }                    
-            }
-        }
     }
 }
